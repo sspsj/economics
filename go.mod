41529b008cbe5b76c62c7eb9156942d66ed9b5d1
Lane Rettig <lanerettig@gmail.com>
2022-07-30 20:58:28 -0400

    Add input params for sim

--- go.mod
+++ go.mod
@@ -2,10 +2,16 @@ module github.com/spacemeshos/economics
 
 go 1.18
 
-require github.com/stretchr/testify v1.8.0
+require (
+	github.com/stretchr/testify v1.8.0
+	github.com/tcnksm/go-input v0.0.0-20180404061846-548a7d7a8ee8
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- sim.go
+++ sim.go
@@ -1,14 +1,79 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/spacemeshos/economics/rewards"
+	"github.com/tcnksm/go-input"
+	"log"
+	"strconv"
+	"time"
 )
 
 func main() {
-	for i := uint32(0); i < 100; i++ {
-		layerReward := rewards.SubsidyByLayer(i)
-		fmt.Println("Layer: ", i, "; Reward: ", layerReward)
+	genesisDate, tickInterval, endLayer := getParams()
+	log.Printf("genesis date is %s\n", genesisDate)
+	log.Printf("tick interval is %d\n", tickInterval)
+	log.Printf("last layer is %d\n", endLayer)
+}
+
+func getParams() (time.Time, uint32, uint32) {
+	// get params
+	ui := &input.UI{}
+	var genesisDate time.Time
+	if genesisDateStr, err := ui.Ask("genesis date (YYYYMMDD)", &input.Options{
+		Default:   "20230101",
+		HideOrder: true,
+		Required:  true,
+		Loop:      true,
+		ValidateFunc: func(s string) (err error) {
+			_, err = time.Parse("20060102", s)
+			return
+		},
+	}); err != nil {
+		log.Fatal(err)
+	} else {
+		genesisDate, _ = time.Parse("20060102", genesisDateStr)
 	}
+
+	defaultTickVal := "2016 (one week)"
+	var tickInterval int
+	if tickIntervalStr, err := ui.Ask("layer tick interval", &input.Options{
+		Default:   defaultTickVal,
+		HideOrder: true,
+		Required:  true,
+		Loop:      true,
+		ValidateFunc: func(s string) (err error) {
+			_, err = strconv.Atoi(s)
+			return
+		},
+	}); err != nil {
+		log.Fatal(err)
+	} else if tickIntervalStr == defaultTickVal {
+		tickInterval = 2016
+	} else {
+		tickInterval, _ = strconv.Atoi(tickIntervalStr)
+	}
+
+	defaultEndLayer := "1051920 (ten years)"
+	var endLayer int
+	if endLayerStr, err := ui.Ask("end layer", &input.Options{
+		Default:   defaultEndLayer,
+		HideOrder: true,
+		Required:  true,
+		Loop:      true,
+		ValidateFunc: func(s string) (err error) {
+			_, err = strconv.Atoi(s)
+			return
+		},
+	}); err != nil {
+		log.Fatal(err)
+	} else if endLayerStr == defaultEndLayer {
+		endLayer = 1051920
+	} else {
+		endLayer, _ = strconv.Atoi(endLayerStr)
+	}
+
+	return genesisDate, uint32(tickInterval), uint32(endLayer)
+	//for i := uint32(0); i < 100; i++ {
+	//	layerReward := rewards.SubsidyByLayer(i)
+	//	fmt.Println("Layer: ", i, "; Reward: ", layerReward)
+	//}
 }
